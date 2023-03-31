6d0c9e7c98a72740d7165390c1426d052e45a217
lostlemoncurd <109440184+LostLemonCurd@users.noreply.github.com>
2023-03-31 16:48:33 +0200

    Small changes to colours

---
+++ script.js
@@ -0,0 +1,16 @@
+// Better visibility for the return to the top arrow by changing its color.
+
+let arrowUp = document.querySelector('#arrow-up');
+console.log(arrowUp);
+let footer = document.querySelector('#footer');
+console.log(footer);
+function toggleArrowColor() {
+  if (window.scrollY + window.innerHeight > footer.offsetTop) {
+    arrowUp.classList.add('active');
+  } else {
+    arrowUp.classList.remove('active');
+  }
+}
+window.addEventListener('scroll', toggleArrowColor);
+
+
