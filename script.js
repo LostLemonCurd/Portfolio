35df402e32863cee9ee8b7a3ae266a3774345c63
lostlemoncurd <109440184+LostLemonCurd@users.noreply.github.com>
2023-06-09 14:56:49 +0200

    Updated portfolio

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
 // Better visibility for the return to the top arrow by changing its color.
 
-let arrowUp = document.querySelector('#arrow-up');
+let arrowUp = document.querySelector("#arrow-up");
 console.log(arrowUp);
-let footer = document.querySelector('#footer');
+let footer = document.querySelector("#footer");
 console.log(footer);
 function toggleArrowColor() {
   if (window.scrollY + window.innerHeight > footer.offsetTop) {
-    arrowUp.classList.add('active');
+    arrowUp.classList.add("active");
   } else {
-    arrowUp.classList.remove('active');
+    arrowUp.classList.remove("active");
   }
 }
-window.addEventListener('scroll', toggleArrowColor);
+window.addEventListener("scroll", toggleArrowColor);
 
-
+let projetMinus = document.querySelector(".projet-minus");
+projetMinus.addEventListener("click", function () {
+  window.location.href = "/minus.html";
+});
